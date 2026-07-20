f613857262924355cc1426cecd6fafbaed04b3b2
rajendro00 <rajenkundu0000@gmail.com>
2026-07-20 23:58:26 +0600

    update

--- class1/src/components/State.tsx
+++ class1/src/components/State.tsx
@@ -2,6 +2,10 @@ import { use, useState } from "react"
 
 
 
+import Img1 from "../assets/image/clients/client1.png"
+import Img2 from "../assets/image/clients/client2.png"
+import Img3 from "../assets/image/clients/client3.png"
+
 
 export default function useState1(){
   const [count, setCount] = useState(0);
@@ -84,8 +88,20 @@ const [activeTabs2, setActiveTabs2] = useState(tabs2[0])
   },
 
 ];
-  const [activeFaq, setActiveFaq] = useState(null)
+  const [activeFaq, setActiveFaq] = useState(1)
 
+  const [count2, setCount2] = useState(0)
+
+  const [active4, setActive4] = useState(null)
+
+  const colors = ["Red", "Green", "Blue"];
+  const [bgColor, setBgColor] = useState("White");
+  const [fontCount, fontSetCount] = useState(20);
+  const [liked, setLiked] = useState(false)
+
+  const imgData = [Img1, Img2, Img3];
+  const [currenIndex, setCurrenIndex] = useState(0);
+  const [isOnColor, setOnColor] = useState(false)
   return(
     <>
         <div className="flex flex-col items-center justify-center  py-8 gap-2">
@@ -220,7 +236,57 @@ const [activeTabs2, setActiveTabs2] = useState(tabs2[0])
               </div>
             </div>
 
-        
+        {/* counter */}
+        <div className="max-w-3xl mx-auto mt-20 p-6 bg-white rounded-2xl shadow-lg" style={{backgroundColor: bgColor}}  >
+          <h1 className="text-center">{count2}</h1>
+          <div className="flex justify-center gap-3 mt-8">
+            <button onClick={() => setCount2(count2 + 1)} className="px-3 py-3 bg-red-400">+</button>
+            <button onClick={() => setCount2(count2 - 1)} className="px-3 py-3 bg-red-400">-</button>
+            <button onClick={() => setCount2(0)} className="px-3 py-3 bg-red-400">Reset</button>
+          </div>
+
+          {/* Show/Hide */}
+          <div>
+            {}
+            <h2 onClick={() => setActive4(!active4) }>Show</h2>
+            {active4 === true &&(
+              <p>Hello React</p>
+            )}
+          </div>
+
+          <div className="flex justify-center gap-3 mt-12">
+            {colors.map((color, index) => (
+              <button key={index} onClick={() => setBgColor(color) } className="px-4 py-3 bg-blue-400">{color}</button>
+            ))}
+          </div>
+
+          {/* font size */}
+          <div>
+            <h2 style={{fontSize: fontCount}}> React Learning</h2>
+            <div>
+              <button onClick={() => fontSetCount(fontCount + 1)}> A+</button>
+              <button onClick={() => fontSetCount(fontCount - 1)}> A-</button>
+            </div>
+          </div>
+          <div>
+            <button onClick={() => setLiked(!liked)}>
+              { liked ? "❤️ Liked" : "🤍 Like"}
+            </button>
+          </div>
+
+          <div className={`mt-12 ${
+            isOnColor ? "bg-amber-600" : "bg-white"
+          } `}>
+            <div className="flex">
+              <img src={imgData[currenIndex]} alt="" />
+            </div>
+            <button onClick={ () => { currenIndex === 0 ? setCurrenIndex(imgData.length -1 ) : setCurrenIndex(currenIndex -1) }} >prev</button>
+            <button onClick={ () => {setCurrenIndex((currenIndex + 1) % imgData.length) }} >Next</button>
+          </div>
+
+              <button onClick={ () => setOnColor(!isOnColor)}>Dark Mode</button>
+
+        </div>
     </>
   )
 }
